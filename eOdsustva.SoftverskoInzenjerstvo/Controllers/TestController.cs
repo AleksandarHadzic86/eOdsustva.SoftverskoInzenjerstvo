8d20e38dadbfc543985f6344695063d9bfb2a40d
Aleksandar Hadzic <aleksandarhadzic1986@gmail.com>
2025-12-19 12:12:23 +0100

    Add project files.

---
+++ eOdsustva.SoftverskoInzenjerstvo/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using eOdsustva.SoftverskoInzenjerstvo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eOdsustva.SoftverskoInzenjerstvo.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student",
+                DateOfBirth = new DateTime(2000, 1, 1)
+            };
+            return View(data);
+        }
+    }
+}
---
+++ eOdsustva.SoftverskoInzenjerstvo/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eOdsustva.SoftverskoInzenjerstvo.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
